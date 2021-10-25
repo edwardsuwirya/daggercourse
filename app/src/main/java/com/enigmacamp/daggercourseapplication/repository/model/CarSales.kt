444a4924d0e9d5f5c43dcf9ea81d4c569ad79221
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-10-25 12:53:29 +0700

    Remove @inject on CarSales

--- app/src/main/java/com/enigmacamp/daggercourseapplication/repository/model/CarSales.kt
+++ app/src/main/java/com/enigmacamp/daggercourseapplication/repository/model/CarSales.kt
@@ -1,7 +1,5 @@
 package com.enigmacamp.daggercourseapplication.repository.model
 
-import javax.inject.Inject
-
-class CarSales @Inject constructor(private val car: Vehicle) {
+class CarSales(private val car: Vehicle) {
     fun saleCar() = "Sale a car with type : $car"
 }
